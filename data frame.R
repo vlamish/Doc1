d6589c6cafd1c52c959a0deb870582ff98269575
public-health@inbox.ru <public-health@inbox.ru>
2018-07-13 23:27:10 +0300

    Day2

---
+++ data frame.R
@@ -0,0 +1,7 @@
+population <- c(100, 150, 250, 300)
+time <- c(1, 2, 3, 4)
+df <- data.frame(population, date)
+
+names(df) <- c("craccivora", "days")
+
+craccivora <- data.frame(population = c(400, 350, 250, 200), days = c(1, 2, 3, 4))
--- first script.R
+++ first script.R
@@ -1,4 +1,7 @@
 ggplot() +
     geom_blank() +
     ggtitle('Population dynamics', subtitle = 'craccivora') +
-    scale_x_continuous(name = 'days', limits = c(0,20))
+    scale_x_continuous(name = 'days', limits = c(0,20)) +
+    scale_y_continuous(name = 'Population', limits = c(0,100))
+                       
+                       
