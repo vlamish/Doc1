0c2dfc79ed91314b8918f0afd9d016dfab80534e
public-health@inbox.ru <public-health@inbox.ru>
2018-07-25 21:02:52 +0300

    quite good

--- second trying to get ready for data frame.r
+++ second trying to get ready for data frame.r
@@ -1,21 +1,21 @@
 library(ggplot2)
 library(scales)
 
-df.pest <- data.frame(population = c(100,200, 300), time = c("2018-05-01", "2018-05-15", "2018-07-01"))
-df.ne <- data.frame(population = c(95, 120, 50), time = c("2018-05-01", "2018-05-15", "2018-07-01"))
+df.pest <- data.frame(population = c(100,200, 300),
+                      time = c('2018-05-01', '2018-05-15', '2018-07-01'),
+                      species = c('enemy', 'enemy', 'enemy'))
 
-df.pest$time <- as.Date(df.pest$time)
-df.ne$time <- as.Date(df.ne$time)
+df.ne <- data.frame(population = c(95, 120, 50),
+                    time = c('2018-05-01', '2018-05-15', '2018-07-01'),
+                    species = c("pest", "pest", "pest"))
 
-ggplot(df.pest, aes(time, population)) +
-    geom_line() +
-    labs(x = 'period', y = 'number of individuals in a population') +
+total2 <- rbind(df.ne, df.pest)
+total2$time <- as.Date(total2$time)
+
+
+ggplot(total2, aes(x = time, y = population, colour = species)) +
+    geom_line() + 
     ggtitle('Population dynamics', subtitle = 'subtitle unknown') +
     scale_x_date(date_breaks = "1 month", labels = date_format("%b-%Y"))
 
-ggplot(NULL, aes(time, population)) +
-    geom_line(data = df.pest) +
-    geom_line(data = df.ne) +
-    ggtitle('Population dynamics', subtitle = 'subtitle unknown') +
-    scale_x_date(date_breaks = "1 month", labels = date_format("%b-%Y"))
-
+    
